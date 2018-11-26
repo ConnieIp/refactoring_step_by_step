e256cac86d1d6b77bd4f8a78b88a3162de96dace
Connie Ip <connie.ip@cargosmart.com>
2018-11-26 15:20:20 +0800

    remove checkDriver

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -3,10 +3,6 @@ package com.tws.refactoring;
 public class Police {
     private final int validAge = 18;
 
-    public boolean checkDriver(Driver driver) {
-        return (driver.getAge() >= this.validAge);
-    }
-
     public boolean checkDriverAge(Driver driver) {
         return (driver.getAge() >= this.validAge);
     }
