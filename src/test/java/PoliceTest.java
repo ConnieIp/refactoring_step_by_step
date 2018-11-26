282323a187126afdac8f3d340e1b5959653e7adc
Connie Ip <connie.ip@cargosmart.com>
2018-11-26 15:05:00 +0800

    testcase should_check

--- src/test/java/PoliceTest.java
+++ src/test/java/PoliceTest.java
@@ -1,8 +1,14 @@
+import com.tws.refactoring.Driver;
+import com.tws.refactoring.Police;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PoliceTest {
     @Test
     public void should_check_driver(){
-
+        assertTrue(new Police().checkDriver(new Driver(18)));
+        assertFalse(new Police().checkDriver(new Driver(17)));
     }
 }
